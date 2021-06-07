5b3a6f2d763e47d40062f071baa3d0336618bf83
EJFBODO <64859953+EJFBODO@users.noreply.github.com>
2021-06-06 21:25:28 -0300

    primer commit

---
+++ db_agendaEmanuelBodo.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_agenda
+-- ------------------------------------------------------
+-- Server version	8.0.24
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contactos`
+--
+
+DROP TABLE IF EXISTS `contactos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contactos` (
+  `idContactos` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `fechaNacimiento` date DEFAULT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idContactos`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contactos`
+--
+
+LOCK TABLES `contactos` WRITE;
+/*!40000 ALTER TABLE `contactos` DISABLE KEYS */;
+INSERT INTO `contactos` VALUES (1,'Gustavo','Gonzalez','1985-12-17','gustavogonzalez@gmail.com','1559873213'),(2,'Matias','Carrizo','1985-03-28','matiascarrizo@gmail.com','1546753211'),(3,'Angel','Vega','1985-10-31','angelvega@yahoo.com','1554315932'),(4,'Jose','Serio','1984-01-28','joseserio@gmail.com','1561974892'),(5,'Jimena','Lopez','1985-08-31','jimenalopez@yahoo.com','1539324213'),(6,'Gustavo','Bodo','1978-05-12','estudiobodo@gmail.com','1556215721'),(7,'Juan','Gutierrez','1984-08-20','juangutierrez@gmail.com','1559781242'),(8,'Guido','Piccione','1985-06-01','guidopiccione@gmail.com','1565438912'),(9,'Leonardo','Godoy','1984-12-05','leogodoy@gmail.com','1521963211'),(10,'Patricia','Bodo','1992-05-07','patobodo@gmail.com','1556531235'),(13,'Juan','Machado','1983-06-06','juanmachado@gmail.com','1545327890');
+/*!40000 ALTER TABLE `contactos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-06 21:19:03
---
+++ src/dbagendaemanuelbodo/DbAgendaEmanuelBodo.java
@@ -0,0 +1,245 @@
+package dbagendaemanuelbodo;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class DbAgendaEmanuelBodo {
+
+    Connection con;
+
+    private DbAgendaEmanuelBodo() {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_agenda?serverTimezone=UTC", "root", "ejfb");
+
+        } catch (Exception e) {
+            System.err.println("Error " + e);
+        }
+    }
+
+    private static int menu() {
+
+        int seleccion;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(".::AGENDA::.");
+        System.out.println(".::MENU::.");
+        System.out.println("1 - Mostrar contactos");
+        System.out.println("2 - Buscar contacto");
+        System.out.println("3 - Agregar contacto");
+        System.out.println("4 - Editar contacto");
+        System.out.println("5 - Eliminar contacto");
+        System.out.println("6 - Salir");
+
+        System.out.println("Ingrese el numero : ");
+        seleccion = input.nextInt();
+        return seleccion;
+    }
+
+    public static void main(String[] args) {
+
+        int seleccionUsuario = 0;
+
+        do {
+            seleccionUsuario = menu();
+
+            switch (seleccionUsuario) {
+                case 1:
+                    mostrar();
+                    break;
+                case 2:
+                    buscar();
+                    break;
+                case 3:
+                    agregar();
+                    break;
+                case 4:
+                    editar();
+                    break;
+                case 5:
+                    eliminar();
+                    break;
+
+            }
+        } while (seleccionUsuario != 6);
+
+    }
+
+    private static void mostrar() {
+        DbAgendaEmanuelBodo mt = new DbAgendaEmanuelBodo();
+        Statement st;
+        ResultSet rs;
+
+        try {
+
+            st = mt.con.createStatement();
+            rs = st.executeQuery("SELECT * FROM contactos");
+            System.out.printf("%15s %-15s %-20s %15s %25s %n", "NOMBRE", "APELLIDO", "FECHA DE NACIMIENTO", "EMAIL", "TELEFONO");
+
+            while (rs.next()) {
+                System.out.printf("%15s %-15s %-20s %15s %20s %n", rs.getString("nombre"), rs.getString("apellido"), rs.getDate("fechaNacimiento"), rs.getString("email"), rs.getString("telefono"));
+
+            }
+
+            st = mt.con.createStatement();
+            String Query = "SELECT * from contactos WHERE MONTH(fechaNacimiento)=MONTH(NOW()) AND DAY(fechaNacimiento)=DAY(NOW());";
+            rs = st.executeQuery(Query);
+
+            while (rs.next()) {
+                System.out.println(" Es el cumpleaños de " + rs.getString("nombre") + "  " + rs.getString("apellido"));
+
+            }
+
+            mt.con.close();
+
+        } catch (SQLException e) {
+            System.err.println("ERROR AL OBTENER LOS DATOS");
+        }
+
+    }
+
+    private static void buscar() {
+        DbAgendaEmanuelBodo mt = new DbAgendaEmanuelBodo();
+        Statement st;
+        ResultSet rs;
+
+        try {
+
+            System.out.print("Ingrese contacto a buscar por nombre,apellido o email: ");
+            Scanner input = new Scanner(System.in);
+            String busqueda = input.next();
+
+            st = mt.con.createStatement();
+            rs = st.executeQuery("SELECT * FROM contactos WHERE nombre like'" + busqueda + "' OR apellido like'" + busqueda + "' OR email Like '" + busqueda + "%'");
+            while (rs.next()) {
+                System.out.printf("%-15s %-15s %15s %n", "NOMBRE", "APELLIDO", "EMAIL");
+                System.out.printf("%-15s %-15s %15s %n", rs.getString("nombre"), rs.getString("apellido"), rs.getString("email"));
+            }
+
+            mt.con.close();
+        } catch (SQLException e) {
+            System.err.println("Error");
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    private static void agregar() {
+        DbAgendaEmanuelBodo mt = new DbAgendaEmanuelBodo();
+
+        try {
+            System.out.println("Ingresar separadamente nombre,apellido,fecha de nacimiento sin barra(YYYY/MM/DD),email y telefono:");
+
+            Scanner input = new Scanner(System.in);
+
+            String query = "insert into contactos(nombre,apellido,fechaNacimiento,email,telefono)" + "values (?,?,?,?,?)";
+            PreparedStatement ps = mt.con.prepareStatement(query);
+            String nombre = input.next();
+            ps.setString(1, nombre);
+            String apellido = input.next();
+            ps.setString(2, apellido);
+            int fechaNacimiento = input.nextInt();
+            ps.setInt(3, fechaNacimiento);
+            String email = input.next();
+            ps.setString(4, email);
+            String telefono = input.next();
+            ps.setString(5, telefono);
+
+            int resultCount = ps.executeUpdate();
+            System.out.println("número de contactos agregados->" + resultCount);
+
+            mt.con.close();
+
+        } catch (SQLException e) {
+            System.err.println("Error");
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    private static void editar() {
+        DbAgendaEmanuelBodo mt = new DbAgendaEmanuelBodo();
+        Statement st;
+        ResultSet rs;
+        try {
+
+            System.out.print("Ingrese apellido del contacto a editar: ");
+            Scanner input = new Scanner(System.in);
+            String busqueda = input.next();
+
+            st = mt.con.createStatement();
+            rs = st.executeQuery("SELECT * FROM contactos WHERE apellido like'" + busqueda + "%'");
+            while (rs.next()) {
+                System.out.printf("%-15s %-15s %15s %n", "NOMBRE", "APELLIDO", "EMAIL");
+                System.out.printf("%-15s %-15s %15s %n", rs.getString("nombre"), rs.getString("apellido"), rs.getString("email"));
+            }
+
+            System.out.println("Ingresar separadamente nombre,apellido,fecha de nacimiento sin barra(YYYY/MM/DD),email y telefono con los nuevos datos:");
+
+            String query = "UPDATE contactos SET nombre=?,apellido=?,fechaNacimiento=?,email=?,telefono=? WHERE nombre='" + busqueda + "' OR apellido='" + busqueda + "' OR email Like '" + busqueda + "%'";
+
+            PreparedStatement ps = mt.con.prepareStatement(query);
+            String nombre = input.next();
+            ps.setString(1, nombre);
+            String apellido = input.next();
+            ps.setString(2, apellido);
+            int fechaNacimiento = input.nextInt();
+            ps.setInt(3, fechaNacimiento);
+            String email = input.next();
+            ps.setString(4, email);
+            String telefono = input.next();
+            ps.setString(5, telefono);
+
+            int resultRowCount = ps.executeUpdate();
+
+            if (resultRowCount > 0) {
+                System.out.println("número de contactos actualizados -> " + resultRowCount);
+            } else {
+                System.out.println("No se modifico ningun registro");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error");
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    private static void eliminar() {
+        DbAgendaEmanuelBodo mt = new DbAgendaEmanuelBodo();
+        Statement st;
+        ResultSet rs;
+        try {
+            System.out.print("Ingrese apellido del contacto a buscar para eliminar: ");
+            Scanner input = new Scanner(System.in);
+            String busqueda = input.next();
+
+            st = mt.con.createStatement();
+            rs = st.executeQuery("SELECT * FROM contactos WHERE apellido like'" + busqueda + "%'");
+            while (rs.next()) {
+                System.out.printf("%-15s %-15s %15s %n", "NOMBRE", "APELLIDO", "EMAIL");
+                System.out.printf("%-15s %-15s %15s %n", rs.getString("nombre"), rs.getString("apellido"), rs.getString("email"));
+
+                System.out.println("Ingresar apellido del contacto a eliminar");
+
+                String query = "DELETE FROM contactos WHERE apellido=?";
+
+                PreparedStatement ps = mt.con.prepareStatement(query);
+                String apellido = input.next();
+                ps.setString(1, apellido);
+
+                int resultCount = ps.executeUpdate();
+                System.out.println("número de contactos eliminados->" + resultCount);
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+}
